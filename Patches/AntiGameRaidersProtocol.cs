2c764572c9e9908411610ad20f2e1f6eb41209c5
Arnimox <justreqofficial@gmail.com>
2025-08-05 23:35:55 -0400

    Add Cheat Sheet to AGR Protocol

--- Patches/AntiGameRaidersProtocol.cs
+++ Patches/AntiGameRaidersProtocol.cs
@@ -1,4 +1,5 @@
-﻿using DragonLens;
+﻿using CheatSheet.Menus;
+using DragonLens;
 using DragonLens.Content.Tools.Spawners;
 using MarioLand.Common.Players;
 using Mono.Cecil.Cil;
@@ -37,20 +38,30 @@ internal sealed class AntiGameRaidersProtocol : BasePatch
         DragonLensPatch(typeof(ProjectileButton), "SafeClick", "proj", true);
         DragonLensPatch(typeof(ProjectileButton), "SafeRightClick", "proj", true);
         // DragonLens:SoundButton
-        DragonLensPatch(typeof(SoundButton), "SafeDraw", "sound");
-        DragonLensPatch(typeof(SoundButton), "SafeClick", "sound", true);
+        DragonLensPatch(typeof(SoundButton), "SafeDraw", "Sound");
+        DragonLensPatch(typeof(SoundButton), "SafeClick", "Sound", true);
         // DragonLens:TileButton
         DragonLensPatch(typeof(TileButton), "SafeDraw", "tile");
         DragonLensPatch(typeof(TileButton), "SafeClick", "tile", true);
+
+        // CheatSheet:Slot
+        MonoModHooks.Modify(typeof(Slot).GetMethod("Init", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance), CheatSheetSlotInit);
+        MonoModHooks.Modify(typeof(Slot).GetMethod("Slot2_onLeftClick", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance), CheatSheetSlotSlot2_onLeftClick);
+        MonoModHooks.Modify(typeof(Slot).GetMethod("Draw"), CheatSheetSlotDraw);
+
+        // CheatSheet:NPCSlot
+        MonoModHooks.Modify(typeof(NPCSlot).GetMethod("Init", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance), CheatSheetNPCSlotInit);
+        MonoModHooks.Modify(typeof(NPCSlot).GetMethod("Slot2_onLeftClick", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance), CheatSheetNPCSlotSlot2_onLeftClick);
+        MonoModHooks.Modify(typeof(NPCSlot).GetMethod("Draw"), CheatSheetNPCSlotDraw);
     }
 
-    readonly static Func<int, bool> isMarioLandBuff = type => ModContent.GetModBuff(type) != null && ModContent.GetModBuff(type).Mod.Name == nameof(MarioLand);
-    readonly static Func<int, bool> isMarioLandDust = dust => ModContent.GetModDust(dust) != null && ModContent.GetModDust(dust).Mod.Name == nameof(MarioLand);
-    readonly static Func<Item, bool> isMarioLandItem = item => item.ModItem != null && item.ModItem.Mod.Name == nameof(MarioLand);
-    readonly static Func<NPC, bool> isMarioLandNPC = npc => npc.ModNPC != null && npc.ModNPC.Mod.Name == nameof(MarioLand);
-    readonly static Func<Projectile, bool> isMarioLandProjectile = proj => proj.ModProjectile != null && proj.ModProjectile.Mod.Name == nameof(MarioLand);
-    readonly static Func<SoundStyle, bool> isMarioLandSound = sound => sound.SoundPath != null && sound.SoundPath.StartsWith(nameof(MarioLand));
-    readonly static Func<int, bool> isMarioLandTile = tile => ModContent.GetModTile(tile) != null && ModContent.GetModTile(tile).Mod.Name == nameof(MarioLand);
+    static bool IsMarioLandBuff(int buff) => ModContent.GetModBuff(buff) != null && ModContent.GetModBuff(buff).Mod.Name == nameof(MarioLand);
+    static bool IsMarioLandDust(Dust dust) => ModContent.GetModDust(dust.type) != null && ModContent.GetModDust(dust.type).Mod.Name == nameof(MarioLand);
+    static bool IsMarioLandItem(Item item) => item.ModItem != null && item.ModItem.Mod.Name == nameof(MarioLand);
+    static bool IsMarioLandNPC(NPC npc) => npc.ModNPC != null && npc.ModNPC.Mod.Name == nameof(MarioLand);
+    static bool IsMarioLandProjectile(Projectile proj) => proj.ModProjectile != null && proj.ModProjectile.Mod.Name == nameof(MarioLand);
+    static bool IsMarioLandSound(SoundStyle sound) => sound.SoundPath != null && sound.SoundPath.StartsWith(nameof(MarioLand));
+    static bool IsMarioLandTile(int tile) => ModContent.GetModTile(tile) != null && ModContent.GetModTile(tile).Mod.Name == nameof(MarioLand);
 
     static void DragonLensPatch(Type type, string methodName, string buttonType, bool showMessage = false)
     {
@@ -59,17 +70,17 @@ internal sealed class AntiGameRaidersProtocol : BasePatch
             ILCursor c = new(il);
 
             c.Emit(OpCodes.Ldarg_0);
-            c.Emit(OpCodes.Ldfld, type.GetField(buttonType == "buff" ? "type" : buttonType, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance));
+            c.Emit(OpCodes.Ldfld, type.GetField(buttonType == "buff" ? "type" : buttonType == "tile" ? "tileType" : buttonType, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance));
 
             c.EmitDelegate((Delegate)(buttonType switch
             {
-                "buff" => isMarioLandBuff,
-                "dust" => isMarioLandDust,
-                "item" => isMarioLandItem,
-                "npc" => isMarioLandNPC,
-                "proj" => isMarioLandProjectile,
-                "sound" => isMarioLandSound,
-                "tile" => isMarioLandTile,
+                "buff" => IsMarioLandBuff,
+                "dust" => IsMarioLandDust,
+                "item" => IsMarioLandItem,
+                "npc" => IsMarioLandNPC,
+                "proj" => IsMarioLandProjectile,
+                "Sound" => IsMarioLandSound,
+                "tile" => IsMarioLandTile,
                 _ => () => false
             }));
 
@@ -81,5 +92,115 @@ internal sealed class AntiGameRaidersProtocol : BasePatch
             c.Emit(OpCodes.Ret);
             c.MarkLabel(label);
         });
+    }
+
+    static void CheatSheetSlotInit(ILContext il)
+    {
+        ILCursor c = new(il);
+
+        c.TryGotoNext(i => i.MatchLdnull());
+        c.Index += 7;
+
+        c.Emit(OpCodes.Ldarg_0);
+        c.Emit(OpCodes.Ldfld, typeof(Slot).GetField("item", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance));
+
+        c.EmitDelegate(IsMarioLandItem);
+
+        var label = c.DefineLabel();
+        c.Emit(OpCodes.Brfalse, label);
+        c.Emit(OpCodes.Ret);
+        c.MarkLabel(label);
+    }
+
+    static void CheatSheetSlotSlot2_onLeftClick(ILContext il)
+    {
+        ILCursor c = new(il);
+
+        c.Emit(OpCodes.Ldarg_0);
+        c.Emit(OpCodes.Ldfld, typeof(Slot).GetField("item", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance));
+
+        c.EmitDelegate(IsMarioLandItem);
+
+        var label = c.DefineLabel();
+        c.Emit(OpCodes.Brfalse, label);
+
+        c.EmitDelegate(MarioLand.ShowGameRaidersProtocolMessage);
+
+        c.Emit(OpCodes.Ret);
+        c.MarkLabel(label);
+    }
+
+    static void CheatSheetSlotDraw(ILContext il)
+    {
+        ILCursor c = new(il);
+
+        c.TryGotoNext(MoveType.After, i => i.MatchLdcI4(0), i => i.MatchLdcR4(out _));
+        c.Index++;
+
+        c.Emit(OpCodes.Ldarg_0);
+        c.Emit(OpCodes.Ldfld, typeof(Slot).GetField("item", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance));
+
+        c.EmitDelegate(IsMarioLandItem);
+
+        var label = c.DefineLabel();
+        c.Emit(OpCodes.Brfalse, label);
+        c.Emit(OpCodes.Ret);
+        c.MarkLabel(label);
+    }
+
+    static void CheatSheetNPCSlotInit(ILContext il)
+    {
+        ILCursor c = new(il);
+
+        c.TryGotoNext(i => i.MatchRet());
+        c.Index -= 10;
+
+        c.Emit(OpCodes.Ldarg_0);
+        c.Emit(OpCodes.Ldfld, typeof(NPCSlot).GetField("npc", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance));
+
+        c.EmitDelegate(IsMarioLandNPC);
+
+        var label = c.DefineLabel();
+        c.Emit(OpCodes.Brfalse, label);
+
+        c.Emit(OpCodes.Ret);
+        c.MarkLabel(label);
+    }
+
+    static void CheatSheetNPCSlotSlot2_onLeftClick(ILContext il)
+    {
+        ILCursor c = new(il);
+
+        c.Emit(OpCodes.Ldarg_0);
+        c.Emit(OpCodes.Ldfld, typeof(NPCSlot).GetField("npc", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance));
+
+        c.EmitDelegate(IsMarioLandNPC);
+
+        var label = c.DefineLabel();
+        c.Emit(OpCodes.Brfalse, label);
+
+        c.EmitDelegate(MarioLand.ShowGameRaidersProtocolMessage);
+
+        c.Emit(OpCodes.Ret);
+        c.MarkLabel(label);
+    }
+
+    static void CheatSheetNPCSlotDraw(ILContext il)
+    {
+        ILCursor c = new(il);
+
+        c.TryGotoNext(i => i.MatchLdcI4(0), i => i.MatchLdcR4(out _));
+        c.Index += 3;
+
+        c.Emit(OpCodes.Ldarg_0);
+        c.Emit(OpCodes.Ldfld, typeof(NPCSlot).GetField("npc", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance));
+
+        c.EmitDelegate(IsMarioLandNPC);
+
+        var label = c.DefineLabel();
+        c.Emit(OpCodes.Brfalse, label);
+
+        c.Emit(OpCodes.Ret);
+        c.MarkLabel(label);
     }
 }
